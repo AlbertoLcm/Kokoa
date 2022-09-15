788df1e1fb968cc8c4346845cb8d2ad49f1f9bbe
AlbertoLcm <alberto.cm4007@gmail.com>
2022-09-15 12:48:47 -0500

    añadiendo segunda ventana

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './pages/Login';
+import MenuLogin from './pages/MenuLogin';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <MenuLogin />
     </div>
   );
 }
---
+++ src/pages/MenuLogin.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MenuLogin() {
+    return(
+        <div>
+            <header>Kokoa</header>
+
+            <div className="contPrincipal">
+                <h2>aqui va el contenido</h2>
+            </div>
+        </div>
+    );
+}
+
+export default MenuLogin;
