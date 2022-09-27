cd0dabb257c4ce42a6703f6fdef914afc15c74cd
EhtanEsquivel <gater__Dragnellle@hotmail.com>
2022-09-27 09:57:00 -0500

    uniendo

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker} from '@react-google-maps/api';
+const google = window.google;
 
 const containerStyle = {
   width: '100%',
   height: '100%'
 };
-
-const center = {
-  lat: -16.946234,
-  lng: -120.831413
-};//16.946234, 120.831413
-
+let center = {
+  lat: 16.946234,
+  lng: 120.831413
+}
+let ubicarGlobo = () => {
+  let currentloc = {
+    lat: 0,
+    lng: 0
+  }
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position) => {
+      currentloc.lat = position.coords.latitude;
+      currentloc.lng = position.coords.longitude;
+    });
+  }else{
+    alert("Tu navegador no soporta el localizarte");
+  }
+  return currentloc;
+}
+const centro = ubicarGlobo();
+alert(centro);
 function Mapa() {
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyC9solbk3q4EuYuef97FhGJJnDAD83jvAs"
@@ -19,25 +36,27 @@ function Mapa() {
 
   const [map, setMap] = React.useState(null)
 
+
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
+    
     setMap(map)
   }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null)
   }, [])
-
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={11}
+        zoom={16}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
+        { 
+        <Marker onLoad={onLoad} position={centro} title={"Por fin"}/>
+        /* Child components, such as markers, info windows, etc. */ }
         <></>
       </GoogleMap>
   ) : <></>
