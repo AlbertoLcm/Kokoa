3cd3e817884c6d6228003c8649d8dd47ea5ad8e8
Luis Alberto <alberto.cm4007@gmail.com>
2022-09-29 11:01:45 -0500

    desplegando

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://kokoatec.herokuapp.com/api'
-    baseURL: 'http://localhost:8081/api'
+  // en producción
+    baseURL: 'https://kokoa-api.herokuapp.com//api'
+    // baseURL: 'http://localhost:8081/api' 
   });
 
 export default instance;
