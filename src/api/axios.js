d3043f547cbda369bcbc0e1834186657317a3ca2
Luis Alberto <alberto.cm4007@gmail.com>
2022-10-06 14:57:20 -0500

    cambiado a host

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   // * en producción
-  // baseURL: "https://kokoa-server.herokuapp.com/api",
+  baseURL: "https://kokoa-server.herokuapp.com/api",
   // * en local
-  baseURL: 'http://localhost:8081/api'
+  // baseURL: 'http://localhost:8081/api'
 });
 
 instance.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
