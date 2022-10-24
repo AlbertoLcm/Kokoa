96d352804458e2b492fe161344d09cf81fac81c5
EhtanEsquivel <gater__Dragnellle@hotmail.com>
2022-10-24 11:14:54 -0500

    Cofpefprin completo

--- src/pages/ConPerf.jsx
+++ src/pages/ConPerf.jsx
@@ -24,8 +24,8 @@ function ConPerf() {
 
     return (
         <div className="contPerf">
-            <Header tipo={'color'} user={user[0].nombre} volver={true} />
-
+            <Header tipo={'responsive'} user={user[0].nombre} volver={true} />
+            
             {
                 opcio && (
                     <div className="acomodo" id="acomodo">
@@ -58,11 +58,11 @@ function ConPerf() {
                                     <img src={backimg} id="confPerfImgBack" />
                                 </div>
                                 <div className="confPerfInfoGen">
-                                    <div id="contInfoGen"><h2>{user[0].nombre}</h2> <button>Cambiar nombre</button></div>
-                                    <div id="contInfoGen"><h2>{user[0].telefono}</h2> <button>Actualizar numero</button></div>
-                                    <div id="contInfoGen"><h2>{user[0].email}</h2> <button>Actualizar numero</button></div>
-                                    <div id="contInfoGen"><h2>{user[0].domicilio !== null ? (user[0].domicilio) : ("Sin dirección")}</h2> <button>Actualizar Dirección</button></div>
-                                    <div id="contInfoGen"><h2>{user[0].fecha_nacimiento !== null ? (user[0].fecha_nacimiento) : ("Sin fecha de nacimiento")}</h2> <button>Actualizar Fecha de nacimiento</button></div>
+                                    <div id="contInfoGen"><h2>{user[0].nombre}</h2> <button><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f3f3f3" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg></button></div>
+                                    <div id="contInfoGen"><h2>{user[0].telefono}</h2> <button><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f3f3f3" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg></button></div>
+                                    <div id="contInfoGen"><h2>{user[0].email}</h2> <button><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f3f3f3" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg></button></div>
+                                    <div id="contInfoGen"><h2>{user[0].domicilio !== null ? (user[0].domicilio) : ("Sin dirección")}</h2> <button><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f3f3f3" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg></button></div>
+                                    <div id="contInfoGen"><h2>{user[0].fecha_nacimiento !== null ? (user[0].fecha_nacimiento) : ("Sin fecha de nacimiento")}</h2> <button><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#f3f3f3" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg></button></div>
                                 </div>
                             </div>
                         ) : cont === 2 ? (
